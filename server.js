13cb5f1996b7158219ee6121fb95bd0bbcfcf60a
Nonsticklotion <pisit676@gmail.com>
2024-06-19 12:21:42 +0700

    finish not found middleware

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const notFoundMiddleWare = require("./middlewares/not-found");
 const app = express();
 
+
 const port = 8000;
+
+app.use(notFoundMiddleWare);
 app.listen(port, () => console.log("server is running in port ", port));
